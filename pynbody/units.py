97768ffea934f80522675a5007e4a56889753f7b
Ben Keller <malzraa@gmail.com>
2026-09-06 08:35:22 -0500

    Because the bases for units are not sorted (as their powers are), certain quantities can be assembled in different orders for different runs (e.g.  g * cm / s vs. cm * g / s).  Since multiplication of floats is not associative, this can lead to nondeterministic behaviour at the ULP level.

--- pynbody/units.py
+++ pynbody/units.py
@@ -670,14 +670,14 @@ class CompositeUnit(UnitBase):
         units, then order the base units by their power (descending)"""
 
         trash = []
-        bases = list(set(self._bases))
+        bases = sorted(set(self._bases), key=str)
         powers = [sum(p for bi, p in zip(self._bases, self._powers)
                        if bi is b)
                   for b in bases]
 
         bp = sorted((x for x in zip(powers, bases) if x[0] != 0),
                     reverse=True,
-                    key=lambda x: x[0])
+                    key=lambda x: (x[0], str(x[1])))
 
         if len(bp) != 0:
             self._powers, self._bases = list(map(list, list(zip(*bp))))
